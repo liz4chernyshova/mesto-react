a1e517935ecf8ecd08f072a619973f7fc56012dd
Liza <liz4chernyshova@yandex.ru>
2021-06-20 11:40:30 +0300

    refactor: 1st review

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,7 +5,7 @@ export default function PopupWithForm(props) {
         <section className={`popup popup-${props.name} ${props.isOpen ? 'popup_opened' : '' }`}>
           <div className="popup__container">
             <button type="button" className="popup__close-btn"><img className="popup__icon" alt="Крестик." src={closeIcon}  onClick={props.onClose} /></button>
-            <form name={props.name} className="form" noValidate>
+            <form name={props.name} className="form">
               <h2 className="form__title">{props.title}</h2>
               {props.children}
               <button type="submit" className="form__save-btn">Сохранить</button>
